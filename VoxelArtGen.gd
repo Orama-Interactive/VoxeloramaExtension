b153b94ed9a45c56bb8ef02b7eb214d89ccb4d31
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2021-10-22 23:59:54 +0300

    Add layer support

--- VoxelArtGen.gd
+++ VoxelArtGen.gd
@@ -2,7 +2,8 @@ extends MeshInstance
 
 
 var cubes := [] # Array of Cube(s)
-var image_tex = preload("icon.png")
+#var image_textures := [preload("res://layers/1.png"), preload("res://layers/2.png"), preload("res://layers/3.png")]
+var image_textures := [preload("res://icon.png")]
 
 onready var camera: Camera = $"../Camera"
 
@@ -19,6 +20,12 @@ class Cube extends Reference:
 	var depth := 1
 	var z_back := 0
 	var z_front := z_back + depth
+
+
+	func _init(_z_back := 0, _depth := 1) -> void:
+		z_back = _z_back
+		depth = _depth
+		z_front = z_back + depth
 
 
 	func generate_faces() -> void:
@@ -106,60 +113,66 @@ func _ready() -> void:
 
 func generate_mesh() -> void:
 	var start: = OS.get_ticks_msec()
-	var image: Image = image_tex.get_data()
-	image.flip_y()
-#	camera.translation.y = max(image.get_size().x, image.get_size().y)
-#	camera.translation.x = image.get_size().x / 2
-#	camera.translation.z = image.get_size().y / 2
+	var array_mesh := ArrayMesh.new()
+	var i := 0
+	for image_tex in image_textures:
+		var image: Image = image_tex.get_data()
+		image.flip_y()
+		camera.translation.y = image.get_size().y / 2
+		camera.translation.x = image.get_size().x / 2
+		camera.translation.z = max(image.get_size().x, image.get_size().y)
 
-	var current_cube := Cube.new()
-	current_cube.start_point = Vector2(0, 0)
-	current_cube.end_point = Vector2(0, 0)
+		var current_cube := Cube.new(i)
+		current_cube.start_point = Vector2(0, 0)
+		current_cube.end_point = Vector2(0, 0)
 
-	image.lock()
-	for x in image.get_size().x:
-		if current_cube.start_point == current_cube.end_point:
-			current_cube = Cube.new()
-			current_cube.start_point = Vector2(x, 0)
-			current_cube.end_point = Vector2(x, 0)
+		image.lock()
+		for x in image.get_size().x:
+			if current_cube.start_point == current_cube.end_point:
+				current_cube = Cube.new(i)
+				current_cube.start_point = Vector2(x, 0)
+				current_cube.end_point = Vector2(x, 0)
 
-		for y in image.get_size().y:
-			var color: Color = image.get_pixel(x, y)
-			if color.a <= 0.1:
-				if current_cube.start_point != current_cube.end_point:
-					cubes.append(current_cube)
+			for y in image.get_size().y:
+				var color: Color = image.get_pixel(x, y)
+				if color.a <= 0.1:
+					if current_cube.start_point != current_cube.end_point:
+						cubes.append(current_cube)
 
-				current_cube = Cube.new()
-				current_cube.start_point = Vector2(x, y+1)
-				current_cube.end_point = Vector2(x, y+1)
-			else:
-				current_cube.end_point = Vector2(x, y+1)
+					current_cube = Cube.new(i)
+					current_cube.start_point = Vector2(x, y+1)
+					current_cube.end_point = Vector2(x, y+1)
+				else:
+					current_cube.end_point = Vector2(x, y+1)
 
-	image.unlock()
-	if current_cube.start_point != current_cube.end_point and !cubes.has(current_cube):
-		cubes.append(current_cube)
+		image.unlock()
+		if current_cube.start_point != current_cube.end_point and !cubes.has(current_cube):
+			cubes.append(current_cube)
 
-	for cube in cubes:
-		cube.generate_faces()
-		cube.generate_uvs(image.get_size())
+		for cube in cubes:
+			cube.generate_faces()
+			cube.generate_uvs(image.get_size())
 
-	var st: = SurfaceTool.new()
+		var st: = SurfaceTool.new()
+		st.begin(Mesh.PRIMITIVE_TRIANGLES)
+		for cube in cubes:
+			cube.draw_cube(st)
 
-	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-	for cube in cubes:
-		cube.draw_cube(st)
+		st.generate_normals()
+#		st.index()
+		array_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, st.commit_to_arrays())
+		var image_texture := ImageTexture.new()
+		image_texture.create_from_image(image, 0)
+		var mat := SpatialMaterial.new()
+		mat.albedo_texture = image_texture
+		array_mesh.surface_set_material(i, mat)
+		array_mesh.surface_set_name(i, "Layer %s" % i)
+		cubes.clear()
 
-	st.generate_normals()
-	st.index()
+		i += 1
+
 	# Commit to a mesh.
-	mesh = st.commit()
-
-	var image_texture := ImageTexture.new()
-	image_texture.create_from_image(image, 0)
-	var mat := SpatialMaterial.new()
-	mat.albedo_texture = image_texture
-	mesh.surface_set_material(0, mat)
-
+	mesh = array_mesh
 	var end: = OS.get_ticks_msec()
 	print("Cubes: ", cubes.size())
 	print("Mesh generated in ", end - start, " ms")
@@ -169,75 +182,84 @@ func generate_mesh() -> void:
 # https://github.com/lawnjelly/GodotTweaks/blob/master/ObjExport/ObjExport.gd#L33
 # and
 # https://github.com/mohammedzero43/CSGExport-Godot/blob/master/addons/CSGExport/csgexport.gd#L48
-func export_obj():
+func export_obj() -> void:
 	var start: = OS.get_ticks_msec()
-	var path:String = "user://%s" % image_tex.resource_path.get_basename().get_file()
+#	var path:String = "user://%s" % image_textures[0].resource_path.get_basename().get_file()
+	var path:String = "user://spider"
 	var file_name: String = path.get_file()
 	var objcont = "" #.obj content
 	var matcont = "" #.mat content
-
-	var mdt = MeshDataTool.new()
-	mdt.create_from_surface(mesh, 0)
-
-	var nVerts = mdt.get_vertex_count()
-	if nVerts == 0:
-		printerr("ObjExport::export : nVerts is 0, aborting")
-		return
-
-	var nFaces = mdt.get_face_count()
+	var vertices_total := 0
 
 	objcont += "# Voxelorama\n"
-	objcont += "# Number of vertices " + str(nVerts) + "\n"
-	objcont += "# Number of faces " + str(nFaces) + "\n"
+#	objcont += "# Number of vertices " + str(nVerts) + "\n"
+#	objcont += "# Number of faces " + str(nFaces) + "\n"
 
 	objcont += "mtllib "+ file_name + ".mtl\n"
 	objcont += "o " + file_name  + "\n"
 
-	var vertcont := ""
-	var uvcont := ""
-	var normalcont := ""
-	# positions
-	for i in range (nVerts):
-		var vertex: Vector3 = mdt.get_vertex(i)
-		vertcont += str("v ", vertex.x, " ", vertex.y, " ", vertex.z, "\n")
+	for s in mesh.get_surface_count():
+		var mdt: = MeshDataTool.new()
+		var err: = mdt.create_from_surface(mesh, s)
+		if err != OK:
+			printerr("Error code: %s" % err)
+			return
 
-		var uv: Vector2 = mdt.get_vertex_uv(i)
-		uvcont += str("vt ", uv.x, " ", uv.y, "\n")
+		var nVerts = mdt.get_vertex_count()
+		if nVerts == 0:
+			printerr("ObjExport::export : nVerts is 0, aborting")
+			return
 
-		var norm: Vector3 = mdt.get_vertex_normal(i)
-		norm = norm.normalized()
-		normalcont += str("vn ", norm.x, " ", norm.y, " ", norm.z, "\n")
+		var nFaces = mdt.get_face_count()
 
-	objcont += vertcont + uvcont + normalcont
+		var vertcont := ""
+		var uvcont := ""
+		var normalcont := ""
+		# positions
+		for i in range (nVerts):
+			var vertex: Vector3 = mdt.get_vertex(i)
+			vertcont += str("v ", vertex.x, " ", vertex.y, " ", vertex.z, "\n")
 
-	var mat = mesh.surface_get_material(0)
-	objcont += "usemtl " + str(mat) + "\n"
+			var uv: Vector2 = mdt.get_vertex_uv(i)
+			uvcont += str("vt ", uv.x, " ", uv.y, "\n")
 
-	for f in range (nFaces):
-		objcont += "f"
-		for i in range (3):
-			# obj expects face vertices in opposite winding order to godot
-			var ind: int = mdt.get_face_vertex(f, 2-i)
+			var norm: Vector3 = mdt.get_vertex_normal(i)
+			norm = norm.normalized()
+			normalcont += str("vn ", norm.x, " ", norm.y, " ", norm.z, "\n")
 
-			# plus one based in obj file
-			ind += 1
-			# vertex, uv, norm
-			objcont += " " + str(ind) + "/" + str(ind)
+		objcont += vertcont + uvcont + normalcont
 
-		objcont += "\n"
+		objcont += "g surface" + str(s) + "\n"
+		var mat: = mesh.surface_get_material(s)
+		objcont += "usemtl " + str(mat) + "\n"
 
-	matcont+=str("newmtl "+str(mat))+'\n'
-	matcont+=str("Kd ",mat.albedo_color.r," ",mat.albedo_color.g," ",mat.albedo_color.b)+'\n'
-	matcont+=str("Ke ",mat.emission.r," ",mat.emission.g," ",mat.emission.b)+'\n'
-	matcont+=str("d ",mat.albedo_color.a)+"\n"
+		for f in range (nFaces):
+			objcont += "f"
+			for i in 3:
+				# obj expects face vertices in opposite winding order to godot
+				var ind: int = mdt.get_face_vertex(f, 2-i)
 
-	## Export texture (taken from Variable's Asset Maker)
-	if mat.albedo_texture != null:
+				# plus one based in obj file
+				ind += 1
+				# vertex, uv, norm
+				objcont += " " + str(ind + vertices_total) + "/" + str(ind + vertices_total)
+
+			objcont += "\n"
+
+		vertices_total += nVerts
+
+		matcont+=str("newmtl "+str(mat))+'\n'
+		matcont+=str("Kd ",mat.albedo_color.r," ",mat.albedo_color.g," ",mat.albedo_color.b)+'\n'
+		matcont+=str("Ke ",mat.emission.r," ",mat.emission.g," ",mat.emission.b)+'\n'
+		matcont+=str("d ",mat.albedo_color.a)+"\n"
+
+		# Export texture taken from https://github.com/Variable-ind/Asset-Maker/blob/master/UI/Scripts/Export.gd
+		if mat.albedo_texture != null:
 			var img_texture :Image = mat.albedo_texture.get_data()
 			img_texture.flip_y()
-			var image_filename: String = path + ".png"
+			var image_filename: String = path + "_%s.png" % s
 			var _error = img_texture.save_png(image_filename)
-			matcont += "map_Kd " + file_name + ".png\n"
+			matcont += "map_Kd " + file_name + "_%s.png\n" % s
 
 	var fi: = File.new()
 # warning-ignore:return_value_discarded
