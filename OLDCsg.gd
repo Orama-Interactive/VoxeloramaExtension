acdd4630a3970bcacf81b2dd68aa8608f62e8299
Manolis Papadeas <35376950+OverloadedOrama@users.noreply.github.com>
2021-10-22 04:05:58 +0300

    Init - Basic mesh rendering and obj exporting

---
+++ OLDCsg.gd
@@ -0,0 +1,202 @@
+extends Spatial
+
+
+var image_tex = preload("icon.png")
+
+#onready var voxel_parent: Spatial = $VoxelParent
+onready var csg_combiner: CSGCombiner = $CSGCombiner
+onready var camera: Camera = $Camera
+
+
+#func _ready() -> void:
+#	var image: Image = image_tex.get_data()
+#	camera.translation.y = max(image.get_size().x, image.get_size().y)
+#	camera.translation.x = image.get_size().x / 2
+#	camera.translation.z = image.get_size().y / 2
+#
+#	var mat := SpatialMaterial.new()
+#	mat.flags_transparent = true
+#	mat.albedo_texture = image_tex
+#	var csg_box := CSGBox.new()
+#
+#	csg_box.width = image.get_size().x
+#	csg_box.depth = 1
+#	csg_box.height = image.get_size().y
+#	csg_box.translation.x = csg_box.width / 2
+#	csg_box.translation.y = csg_box.height / 2
+#	csg_box.material = mat
+#	csg_combiner.add_child(csg_box)
+
+
+func _generate_mesh() -> void:
+	var start: = OS.get_ticks_msec()
+	var image: Image = image_tex.get_data()
+	camera.translation.y = max(image.get_size().x, image.get_size().y)
+	camera.translation.x = image.get_size().x / 2
+	camera.translation.z = image.get_size().y / 2
+
+	var dict := {}
+	image.lock()
+	for x in image.get_size().x:
+		var prev_color = Color(0)
+		var big_width := 1
+		var y_start := 1
+		for y in image.get_size().y:
+			var color : Color = image.get_pixel(x, y)
+			if color.a <= 0.01:
+				continue
+
+			if !dict.has(color):
+				var material: = SpatialMaterial.new()
+				material.flags_transparent = true
+				material.albedo_color = color
+				dict[color] = material
+
+			if color.is_equal_approx(prev_color):
+				big_width += 1
+				if y < image.get_size().y - 1:
+					continue
+			else:
+				print(x, " ", y)
+				if dict.has(prev_color) and big_width > 1:
+					var csg_box := CSGBox.new()
+					csg_box.width = 1
+					csg_box.height = 1
+					csg_box.depth = big_width
+					csg_box.material = dict[prev_color]
+
+					csg_box.translation.x = x + csg_box.width / 2
+					csg_box.translation.z = y_start + csg_box.height / 2
+					csg_combiner.add_child(csg_box)
+
+				big_width = 1
+				prev_color = color
+				y_start = y
+
+			var csg_box := CSGBox.new()
+			csg_box.width = 1
+			csg_box.height = 1
+			csg_box.depth = big_width
+			csg_box.material = dict[color]
+
+			csg_box.translation.x = x + csg_box.width / 2
+			csg_box.translation.z = y_start + csg_box.height / 2
+			csg_combiner.add_child(csg_box)
+
+#			var color : Color = image.get_pixel(x, y)
+#			var material: = SpatialMaterial.new()
+#			material.flags_transparent = true
+#			material.albedo_color = color
+#			var cube: MeshInstance = cube_scene.instance()
+#			cube.translation.x = x
+#			cube.translation.z = y
+#			cube.set_surface_material(0, material)
+#			voxel_parent.add_child(cube)
+	image.unlock()
+
+	var end: = OS.get_ticks_msec()
+	print("CSG Generated in ", end - start, " ms")
+	yield(get_tree(), "idle_frame")
+#	save_obj_file()
+
+
+func save_obj_file() -> void:
+	var start: = OS.get_ticks_msec()
+	# Variables
+	var path:String = "user://%s" % image_tex.resource_path.get_basename().get_file()
+	var objcont = "" #.obj content
+	var matcont = "" #.mat content
+	var vertcount = 0
+	var csgMesh = csg_combiner.get_meshes()
+
+	# OBJ Headers
+	objcont += "mtllib "+path.get_file() + ".mtl\n"
+	objcont += "o ./" + path.get_file() + "\n";
+
+	# Blank material
+#	var blank_material = SpatialMaterial.new()
+#	blank_material.resource_name = "BlankMaterial"
+	#Get surfaces and mesh info
+#	for t in range(voxel_parent.get_child_count()):
+	for t in range(csgMesh[-1].get_surface_count()):
+#		var voxel_instance: MeshInstance = voxel_parent.get_child(t)
+#		var mat : SpatialMaterial = voxel_instance.get_surface_material(0)
+		var mat : SpatialMaterial = csgMesh[-1].surface_get_material(t)
+#		if (mat == null):
+#			print("e")
+#			mat = blank_material
+#		var voxel_mesh: Mesh = voxel_parent.get_child(t).mesh
+#		var surface = voxel_mesh.surface_get_arrays(0)
+		var surface = csgMesh[-1].surface_get_arrays(t)
+		var verts = surface[0]
+		var UVs = surface[4]
+		var normals = surface[1]
+
+		var faces = []
+
+		#create_faces_from_verts (Triangles)
+		var tempv=0
+		for v in range(verts.size()):
+			if tempv%3==0:
+				faces.append([])
+			faces[-1].append(v+1)
+			tempv+=1
+			tempv= tempv%3
+
+		#add verticies
+		var tempvcount =0
+		for ver in verts:
+			objcont+=str("v ",ver[0],' ',ver[1],' ',ver[2])+"\n"
+			tempvcount +=1
+
+		#add UVs
+		for uv in UVs:
+			objcont+=str("vt ",uv[0],' ',uv[1])+"\n"
+		for norm in normals:
+			objcont+=str("vn ",norm[0],' ',norm[1],' ',norm[2])+"\n"
+
+		#add groups and materials
+		objcont+="g surface"+str(t)+"\n"
+
+		objcont+="usemtl "+str(mat)+"\n"
+
+		#add faces
+		for face in faces:
+			objcont+=str("f ", face[2]+vertcount,"/",face[2]+vertcount,"/",face[2]+vertcount,
+			' ',face[1]+vertcount,"/",face[1]+vertcount,"/",face[1]+vertcount,
+			' ',face[0]+vertcount,"/",face[0]+vertcount,"/",face[0]+vertcount)+"\n"
+		#update verts
+		vertcount+=tempvcount
+
+		matcont+=str("newmtl "+str(mat))+'\n'
+		matcont+=str("Kd ",mat.albedo_color.r," ",mat.albedo_color.g," ",mat.albedo_color.b)+'\n'
+		matcont+=str("Ke ",mat.emission.r," ",mat.emission.g," ",mat.emission.b)+'\n'
+		matcont+=str("d ",mat.albedo_color.a)+"\n"
+
+	#Write to files
+	var objfile = File.new()
+	objfile.open(path + ".obj", File.WRITE)
+	objfile.store_string(objcont)
+	objfile.close()
+
+	var mtlfile = File.new()
+	mtlfile.open(path + ".mtl", File.WRITE)
+	mtlfile.store_string(matcont)
+	mtlfile.close()
+
+	#output message
+	var end: = OS.get_ticks_msec()
+	print("CSG mesh ", path, " exported in ", end - start, " ms")
+
+
+#var color : Color = image.get_pixel(x, y)
+#			var csg_box := CSGBox.new()
+#			csg_box.width = 1
+#			csg_box.height = 1
+#			csg_box.depth = 1
+#			csg_box.material = SpatialMaterial.new()
+#			csg_box.material.flags_transparent = true
+#			csg_box.material.albedo_color = color
+#			csg_box.translation.x = x
+#			csg_box.translation.z = y
+#			add_child(csg_box)
---
+++ VoxelArtGen.gd
@@ -0,0 +1,256 @@
+extends MeshInstance
+
+
+var cubes := [] # Array of Cube(s)
+var image_tex = preload("icon.png")
+
+onready var camera: Camera = $"../Camera"
+
+
+class Cube extends Reference:
+	var start_point: = Vector2.ZERO
+	var end_point: = Vector2.ONE
+	var faces: = []
+	var uvs: = [Vector2.ZERO, Vector2(1, 0), Vector2(1, 1), Vector2(0, 1)]
+	var uvs_right: = []
+	var uvs_left: = []
+	var uvs_down: = []
+	var uvs_up: = []
+	var depth := 1
+	var z_back := 0
+	var z_front := z_back + depth
+
+
+	func generate_faces() -> void:
+		end_point += Vector2.RIGHT
+		faces.append([Vector3(start_point.x, start_point.y, z_front), Vector3(end_point.x, start_point.y, z_front), Vector3(end_point.x, end_point.y, z_front), Vector3(start_point.x, end_point.y, z_front), Vector3.FORWARD])
+		faces.append([Vector3(start_point.x, start_point.y, z_back), Vector3(end_point.x, start_point.y, z_back), Vector3(end_point.x, end_point.y, z_back), Vector3(start_point.x, end_point.y, z_back), Vector3.BACK])
+#		faces.append([Vector3(end_point.x, end_point.y, z_front), Vector3(start_point.x, end_point.y, z_front), Vector3(start_point.x, start_point.y, z_front), Vector3(end_point.x, start_point.y, z_front), Vector3.FORWARD])
+#		faces.append([Vector3(end_point.x, end_point.y, z_back), Vector3(start_point.x, end_point.y, z_back), Vector3(start_point.x, start_point.y, z_back), Vector3(end_point.x, start_point.y, z_back), Vector3.BACK])
+
+		faces.append([Vector3(start_point.x, end_point.y, z_back), Vector3(start_point.x, end_point.y, z_front), Vector3(end_point.x, end_point.y, z_front), Vector3(end_point.x, end_point.y, z_back), Vector3.UP])
+		faces.append([Vector3(start_point.x, start_point.y, z_back), Vector3(start_point.x, start_point.y, z_front), Vector3(end_point.x, start_point.y, z_front), Vector3(end_point.x, start_point.y, z_back), Vector3.DOWN])
+
+		faces.append([Vector3(end_point.x, start_point.y, z_back), Vector3(end_point.x, start_point.y, z_front), Vector3(end_point.x, end_point.y, z_front), Vector3(end_point.x, end_point.y, z_back), Vector3.RIGHT])
+		faces.append([Vector3(start_point.x, start_point.y, z_back), Vector3(start_point.x, start_point.y, z_front), Vector3(start_point.x, end_point.y, z_front), Vector3(start_point.x, end_point.y, z_back), Vector3.LEFT])
+
+
+	func generate_uvs(image_size:Vector2) -> void:
+		var start_x := start_point.x / image_size.x
+		var start_y := start_point.y / image_size.y
+		var end_x := end_point.x / image_size.x
+		var end_y := end_point.y / image_size.y
+
+		uvs[0] = Vector2(start_x, start_y)
+		uvs[1] = Vector2(end_x, start_y)
+		uvs[2] = Vector2(end_x, end_y)
+		uvs[3] = Vector2(start_x, end_y)
+
+		uvs_right = [Vector2(end_x, start_y), Vector2(end_x, start_y), Vector2(end_x, end_y), Vector2(end_x, end_y)]
+		uvs_left = [Vector2(start_x, start_y), Vector2(start_x, start_y), Vector2(start_x, end_y), Vector2(start_x, end_y)]
+
+		uvs_down = [Vector2(start_x, start_y), Vector2(start_x, start_y), Vector2(end_x, start_y), Vector2(end_x, start_y)]
+		uvs_up = [Vector2(start_x, end_y), Vector2(start_x, end_y), Vector2(end_x, end_y), Vector2(end_x, end_y)]
+
+
+	func draw_cube(st: SurfaceTool) -> void:
+		for face in faces:
+			var direction: Vector3 = face[4]
+			if direction == Vector3.RIGHT:
+				draw_block_face(st, face, uvs_right)
+			elif direction == Vector3.LEFT:
+				draw_block_face(st, face, uvs_left)
+			elif direction == Vector3.DOWN:
+				draw_block_face(st, face, uvs_down)
+			elif direction == Vector3.UP:
+				draw_block_face(st, face, uvs_up)
+			else:
+				draw_block_face(st, face, uvs)
+
+
+	# https://github.com/godotengine/godot-demo-projects/blob/3.3-f9333dc/3d/voxel/world/chunk.gd#L171
+	func draw_block_face(surface_tool: SurfaceTool, verts, _uvs):
+		var direction: Vector3 = verts[4]
+#		_uvs = [Vector2.ZERO, Vector2(1, 0), Vector2(1, 1), Vector2(0, 1)]
+		if direction == Vector3.BACK or direction == Vector3.DOWN or direction == Vector3.RIGHT:
+			surface_tool.add_uv(_uvs[0]); surface_tool.add_vertex(verts[0])
+			surface_tool.add_uv(_uvs[1]); surface_tool.add_vertex(verts[1])
+			surface_tool.add_uv(_uvs[2]); surface_tool.add_vertex(verts[2])
+
+			surface_tool.add_uv(_uvs[2]); surface_tool.add_vertex(verts[2])
+			surface_tool.add_uv(_uvs[3]); surface_tool.add_vertex(verts[3])
+			surface_tool.add_uv(_uvs[0]); surface_tool.add_vertex(verts[0])
+		else:
+			surface_tool.add_uv(_uvs[2]); surface_tool.add_vertex(verts[2])
+			surface_tool.add_uv(_uvs[1]); surface_tool.add_vertex(verts[1])
+			surface_tool.add_uv(_uvs[0]); surface_tool.add_vertex(verts[0])
+
+			surface_tool.add_uv(_uvs[0]); surface_tool.add_vertex(verts[0])
+			surface_tool.add_uv(_uvs[3]); surface_tool.add_vertex(verts[3])
+			surface_tool.add_uv(_uvs[2]); surface_tool.add_vertex(verts[2])
+
+#		var normal:= Vector3.RIGHT
+#		surface_tool.add_uv(_uvs[0]); surface_tool.add_normal(normal); surface_tool.add_vertex(verts[0])
+#		surface_tool.add_uv(_uvs[1]); surface_tool.add_normal(normal); surface_tool.add_vertex(verts[1])
+#		surface_tool.add_uv(_uvs[2]); surface_tool.add_normal(normal); surface_tool.add_vertex(verts[2])
+#
+#		surface_tool.add_uv(_uvs[2]); surface_tool.add_normal(normal); surface_tool.add_vertex(verts[2])
+#		surface_tool.add_uv(_uvs[3]); surface_tool.add_normal(normal); surface_tool.add_vertex(verts[3])
+#		surface_tool.add_uv(_uvs[0]); surface_tool.add_normal(normal); surface_tool.add_vertex(verts[0])
+
+
+func _ready() -> void:
+	generate_mesh()
+	export_obj()
+
+
+func generate_mesh() -> void:
+	var start: = OS.get_ticks_msec()
+	var image: Image = image_tex.get_data()
+	image.flip_y()
+#	camera.translation.y = max(image.get_size().x, image.get_size().y)
+#	camera.translation.x = image.get_size().x / 2
+#	camera.translation.z = image.get_size().y / 2
+
+	var current_cube := Cube.new()
+	current_cube.start_point = Vector2(0, 0)
+	current_cube.end_point = Vector2(0, 0)
+
+	image.lock()
+	for x in image.get_size().x:
+		if current_cube.start_point == current_cube.end_point:
+			current_cube = Cube.new()
+			current_cube.start_point = Vector2(x, 0)
+			current_cube.end_point = Vector2(x, 0)
+
+		for y in image.get_size().y:
+			var color: Color = image.get_pixel(x, y)
+			if color.a <= 0.1:
+				if current_cube.start_point != current_cube.end_point:
+					cubes.append(current_cube)
+
+				current_cube = Cube.new()
+				current_cube.start_point = Vector2(x, y+1)
+				current_cube.end_point = Vector2(x, y+1)
+			else:
+				current_cube.end_point = Vector2(x, y+1)
+
+	image.unlock()
+	if current_cube.start_point != current_cube.end_point and !cubes.has(current_cube):
+		cubes.append(current_cube)
+
+	for cube in cubes:
+		cube.generate_faces()
+		cube.generate_uvs(image.get_size())
+
+	var st: = SurfaceTool.new()
+
+	st.begin(Mesh.PRIMITIVE_TRIANGLES)
+	for cube in cubes:
+		cube.draw_cube(st)
+
+	st.generate_normals()
+	st.index()
+	# Commit to a mesh.
+	mesh = st.commit()
+
+	var image_texture := ImageTexture.new()
+	image_texture.create_from_image(image, 0)
+	var mat := SpatialMaterial.new()
+	mat.albedo_texture = image_texture
+	mesh.surface_set_material(0, mat)
+
+	var end: = OS.get_ticks_msec()
+	print("Cubes: ", cubes.size())
+	print("Mesh generated in ", end - start, " ms")
+
+
+# Thanks to
+# https://github.com/lawnjelly/GodotTweaks/blob/master/ObjExport/ObjExport.gd#L33
+# and
+# https://github.com/mohammedzero43/CSGExport-Godot/blob/master/addons/CSGExport/csgexport.gd#L48
+func export_obj():
+	var start: = OS.get_ticks_msec()
+	var path:String = "user://%s" % image_tex.resource_path.get_basename().get_file()
+	var file_name: String = path.get_file()
+	var objcont = "" #.obj content
+	var matcont = "" #.mat content
+
+	var mdt = MeshDataTool.new()
+	mdt.create_from_surface(mesh, 0)
+
+	var nVerts = mdt.get_vertex_count()
+	if nVerts == 0:
+		printerr("ObjExport::export : nVerts is 0, aborting")
+		return
+
+	var nFaces = mdt.get_face_count()
+
+	objcont += "# Voxelorama\n"
+	objcont += "# Number of vertices " + str(nVerts) + "\n"
+	objcont += "# Number of faces " + str(nFaces) + "\n"
+
+	objcont += "mtllib "+ file_name + ".mtl\n"
+	objcont += "o " + file_name  + "\n"
+
+	var vertcont := ""
+	var uvcont := ""
+	var normalcont := ""
+	# positions
+	for i in range (nVerts):
+		var vertex: Vector3 = mdt.get_vertex(i)
+		vertcont += str("v ", vertex.x, " ", vertex.y, " ", vertex.z, "\n")
+
+		var uv: Vector2 = mdt.get_vertex_uv(i)
+		uvcont += str("vt ", uv.x, " ", uv.y, "\n")
+
+		var norm: Vector3 = mdt.get_vertex_normal(i)
+		norm = norm.normalized()
+		normalcont += str("vn ", norm.x, " ", norm.y, " ", norm.z, "\n")
+
+	objcont += vertcont + uvcont + normalcont
+
+	var mat = mesh.surface_get_material(0)
+	objcont += "usemtl " + str(mat) + "\n"
+
+	for f in range (nFaces):
+		objcont += "f"
+		for i in range (3):
+			# obj expects face vertices in opposite winding order to godot
+			var ind: int = mdt.get_face_vertex(f, 2-i)
+
+			# plus one based in obj file
+			ind += 1
+			# vertex, uv, norm
+			objcont += " " + str(ind) + "/" + str(ind)
+
+		objcont += "\n"
+
+	matcont+=str("newmtl "+str(mat))+'\n'
+	matcont+=str("Kd ",mat.albedo_color.r," ",mat.albedo_color.g," ",mat.albedo_color.b)+'\n'
+	matcont+=str("Ke ",mat.emission.r," ",mat.emission.g," ",mat.emission.b)+'\n'
+	matcont+=str("d ",mat.albedo_color.a)+"\n"
+
+	## Export texture (taken from Variable's Asset Maker)
+	if mat.albedo_texture != null:
+			var img_texture :Image = mat.albedo_texture.get_data()
+			img_texture.flip_y()
+			var image_filename: String = path + ".png"
+			var _error = img_texture.save_png(image_filename)
+			matcont += "map_Kd " + file_name + ".png\n"
+
+	var fi: = File.new()
+# warning-ignore:return_value_discarded
+	fi.open(path + ".obj", File.WRITE)
+	fi.store_string(objcont)
+	fi.close()
+
+	var mtlfile: = File.new()
+# warning-ignore:return_value_discarded
+	mtlfile.open(path + ".mtl", File.WRITE)
+	mtlfile.store_string(matcont)
+	mtlfile.close()
+
+	#output message
+	var end: = OS.get_ticks_msec()
+	print("Mesh ", path, " exported in ", end - start, " ms")
