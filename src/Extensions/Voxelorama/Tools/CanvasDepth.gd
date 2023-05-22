e63c3406cd7ddb005b5029b3e4930a4188bb966a
Variable <pkfayezakhtar@gmail.com>
2023-05-22 16:01:59 +0500

    add depth distinction by color

--- src/Extensions/Voxelorama/Tools/CanvasDepth.gd
+++ src/Extensions/Voxelorama/Tools/CanvasDepth.gd
@@ -27,9 +27,44 @@ func _draw() -> void:
 			if image.get_pixel(x, y).a == 0:
 				continue
 			var depth_str := str(depth_array[x][y])
-			draw_string(font, Vector2(x, y) * 20 + Vector2.DOWN * 16, depth_str)
+			draw_string(
+				font, Vector2(x, y) * 20 + Vector2.DOWN * 16, depth_str, get_color(depth_array[x][y])
+			)
 	image.unlock()
 	draw_set_transform(position, rotation, scale)
+
+
+func get_color(depth: float):
+	var weight = 0
+	var color_a := Color.red
+	var color_b := Color.white
+	if depth > 0 and depth <= 5:
+		weight = (depth - 1) /5.0
+		color_a = Color.white
+		color_b = Color.blue
+
+	if depth > 5 and depth <= 10:
+		weight = (depth - 6)/25.0
+		color_a = Color.blue
+		color_b = Color.green
+
+	if depth > 10 and depth <= 15:
+		weight = (depth - 11)/25.0
+		color_a = Color.green
+		color_b = Color.yellow
+
+	if depth > 15 and depth <= 20:
+		weight = (depth - 16)/25.0
+		color_a = Color.yellow
+		color_b = Color.orange
+
+	if depth > 20 and depth <= 25:
+		weight = (depth - 26)/25.0
+		color_a = Color.orange
+		color_b = Color.red
+
+	# values larger than 25 onward are in red
+	return color_a.linear_interpolate(color_b, weight)
 
 
 func request_deletion():
