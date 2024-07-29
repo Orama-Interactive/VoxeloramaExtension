11450b77aedd584ad69cd9498ed86577d190743e
Variable <pkfayezakhtar@gmail.com>
2024-07-30 01:26:54 +0500

    added size and shape support

---
+++ src/Tools/EmptyClasses/Project.gd
@@ -0,0 +1,8 @@
+# gdlint: ignore=max-public-methods
+class_name Project
+extends RefCounted
+# A class for project properties.
+
+
+func _init(_frames := [], _name := tr("untitled"), _size := Vector2(64, 64)) -> void:
+	pass
